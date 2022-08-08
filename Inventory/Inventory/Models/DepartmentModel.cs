6f5cd7e0c4a229c7822909fb37ebbc15d450eb92
Ammar Talal <103538665+FatalError98@users.noreply.github.com>
2022-08-08 10:45:39 +0300

    Create DepartmentModel.cs

---
+++ Inventory/Inventory/Models/DepartmentModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class DepartmentModel
+    {
+        private int id;
+        private string department;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+        public string Department
+        {
+            get => department;
+            set => department = value;
+        }
+    }
+}
